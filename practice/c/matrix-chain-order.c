00861b9c92c292faecb2f34bc78572bf9de76408
P Singh Chhaba <PSinghChhaba@yahoo.com>
2018-01-07 16:13:50 +0530

    Added a method with memoisation. It makes the calculation much faster.

--- practice/c/matrix-chain-order.c
+++ practice/c/matrix-chain-order.c
@@ -1,12 +1,15 @@
 #include <stdio.h>
 #include <limits.h>
 
-#define N 5
+#define N 10
 
+int R[128][128] = {{0}};
 int min_mat_chain_order_rec(int *mat, int i, int j) {
 	int k, min=INT_MAX, c;
+	static int count;
 	if (i == j)
 		return 0;
+	printf ("function called %d times\n", ++count);
 
 	for (k=i; k<j; k++) {
 		c = min_mat_chain_order_rec(mat, i, k) +
@@ -16,13 +19,38 @@ int min_mat_chain_order_rec(int *mat, int i, int j) {
 		if (c < min)
 			min = c;
 	}
+	return min;
+}
 
+int min_mat_chain_order_rec_with_memo(int *mat, int i, int j) {
+	int k, min=INT_MAX, c;
+	static int counter;
+
+	if (i == j)
+		return 0;
+
+	if (R[i][j] != 0)
+		return R[i][j];
+
+	printf ("function called %d times\n", ++counter);
+
+	for (k=i; k<j; k++) {
+		c = min_mat_chain_order_rec_with_memo(mat, i, k) +
+			min_mat_chain_order_rec_with_memo(mat, k+1, j) +
+			mat[i-1] * mat[k] * mat[j];
+
+		if (c < min)
+			min = c;
+	}
+	R[i][j] = min;
 	return min;
 }
 
 int main () {
-	int mats[N] = {20, 10, 30, 40, 30};
+	int mats[N] = {20, 10, 30, 40, 30, 15, 20, 10, 50, 60};
 	printf ("Minimum number of multiplication required are %d\n", 
 						min_mat_chain_order_rec(mats, 1, N-1));
+	printf ("Minimum number of multiplication required are %d\n", 
+						min_mat_chain_order_rec_with_memo(mats, 1, N-1));
 }
 
