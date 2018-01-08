b4e6ff4740ff1327caf7065b38957e37fa5d2996
SPritamPal <SPritamPal@BLR-L-SPRITAMPA.microfocus.com>
2018-01-08 05:16:08 +0000

    Printing function call counter in main

--- practice/c/matrix-chain-order.c
+++ practice/c/matrix-chain-order.c
@@ -4,12 +4,14 @@
 #define N 10
 
 int R[128][128] = {{0}};
+static int counter;
+static int count;
 int min_mat_chain_order_rec(int *mat, int i, int j) {
 	int k, min=INT_MAX, c;
-	static int count;
 	if (i == j)
 		return 0;
-	printf ("function called %d times\n", ++count);
+    count++;    
+	//printf ("function called %d times\n", count);
 
 	for (k=i; k<j; k++) {
 		c = min_mat_chain_order_rec(mat, i, k) +
@@ -24,15 +26,15 @@ int min_mat_chain_order_rec(int *mat, int i, int j) {
 
 int min_mat_chain_order_rec_with_memo(int *mat, int i, int j) {
 	int k, min=INT_MAX, c;
-	static int counter;
+
 
 	if (i == j)
 		return 0;
 
 	if (R[i][j] != 0)
 		return R[i][j];
-
-	printf ("function called %d times\n", ++counter);
+    counter++;
+	//printf ("function called %d times\n", ++counter);
 
 	for (k=i; k<j; k++) {
 		c = min_mat_chain_order_rec_with_memo(mat, i, k) +
@@ -50,7 +52,9 @@ int main () {
 	int mats[N] = {20, 10, 30, 40, 30, 15, 20, 10, 50, 60};
 	printf ("Minimum number of multiplication required are %d\n", 
 						min_mat_chain_order_rec(mats, 1, N-1));
+	printf ("function called %d times\n", count);
 	printf ("Minimum number of multiplication required are %d\n", 
 						min_mat_chain_order_rec_with_memo(mats, 1, N-1));
+	printf ("function called %d times\n", ++counter);
 }
 
