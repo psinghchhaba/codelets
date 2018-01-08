0c03601010b479e3ba485a0e7b0828d6620274a3
P Singh Chhaba <PSinghChhaba@yahoo.com>
2018-01-08 19:31:32 +0530

    Added commentary

--- practice/linux-programming/setjmp.c
+++ practice/linux-programming/setjmp.c
@@ -6,6 +6,8 @@ jmp_buf jmpbuffer;
 static int globalval=1;
 
 void fun2() {
+	// Jump back to the jump point which is set in jmpbuffer.
+	// Second arg (2) is the value to which setjmp expression will evaluate
 	longjmp(jmpbuffer, 2);
 }
 void fun1(int i, int j, int k, int l) {
@@ -21,13 +23,17 @@ int main() {
 	register int regival=3;
 	volatile int volaval=4;
 	static int statval=5;
+
+	// Set the jump point.
 	ret = setjmp(jmpbuffer);
 	if (ret != 0) {
+		// Print the values of various variable of different storage classes after longjmp
 		printf ("After longjmp\n");
 		printf ("globalval %d, autoval %d, regival %d, volaval %d, statval %d\n", globalval, autoval, regival, volaval, statval);
 		exit (0);
 	} 
-	
+
+	// Set the values after setjmp
 	globalval = 95;
 	autoval = 96;
 	regival = 97;
